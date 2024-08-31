4dae3f5e96140eaebe74a7f4eaa0dbd32e8f3280
Sanjay Kshirasagar <sanjaykm38@gmail.com>
2024-08-31 15:03:32 +0530

    cpp - initial commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int i = 0;
+    for (i=0;i<10;++i)
+    {
+        cout << i << "\n";
+    }
+
+    cout << "hello world" << "\n";
+
+    return 0;
+}
